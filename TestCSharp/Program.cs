0eca6781a17e7ca6604b2e3ca4c0bece6acb2a45
Javier Carmona <javier.carmona.gallegos@gmail.com>
2025-11-12 20:08:24 -0600

    Agregue mas cambios

--- TestCSharp/Program.cs
+++ TestCSharp/Program.cs
@@ -11,3 +11,10 @@ for (int i = 0; i < 5; i++)
     Console.WriteLine("Hello, World!");
 }
 Console.WriteLine("Hello, World!");
+
+var test = true;
+while (test == true)
+{
+    Console.WriteLine("Hello, World!");
+    test = false;
+}
